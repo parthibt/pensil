ec18d539a6c56fca17a8f4169ba6e5c3dd4b5f8b
parthibt <parthipanbt@gmail.com>
2022-08-17 18:14:36 +0530

    add the files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Heder from './compoents/Heder';
+
+export default function App(){
+    return(
+       <div>
+       <Heder/>
+       </div>
+    )
+}
---
+++ src/compoents/Heder.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <p>hi gays</p>
+    )
+
+}
