bc2445803269dec2bae5165c0c37c3f1e314ab37
parthibt <parthipanbt@gmail.com>
2022-08-18 13:07:07 +0530

    add the files

--- src/compoents/Header.js
+++ src/compoents/Header.js
@@ -3,8 +3,23 @@ import images from '../images/pensil_logo.svg'
 export default function Header(){
     return(
         <div className="hero">
-            <img src={images} alt='' />
+            <div className="logo">
+            <img className="logo_img" src={images} alt='' />
+            </div>
+            <div className="hero_button">
+                <a href="hero" className="link">Use case <i class="fa-solid fa-angle-down"></i>
+                    <div className="link_hover">
+                        <a href="#">Cohort Based courses</a>
+                        <a href="#">Startup or Brand Community</a>
+                        <a href="#">Mentors or Coaches</a>
+                    </div>
+                </a>
+                <a href="hero" className="link">Resources <i class="fa-solid fa-angle-down"></i></a>
+                <a href="hero" className="link">Pricing <i class="fa-solid fa-angle-down"></i></a>
+                <a href="hero" className="link btn_login">Log In</a>
+                <a href="hero" className=" link btn_try">Try for Free</a>
 
+            </div>
         </div>
     )
 
