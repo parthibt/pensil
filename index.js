a08807b6cf0eacfd7137d7cbcc716d62126de0fd
parthibt <parthipanbt@gmail.com>
2022-08-23 14:38:48 +0530

    add the desgins

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function navBars(){
     nav.classList.toggle('active');
       
 }
+function droptown_menu(){
+    const droptownContent = document.querySelector('.inside_list');
+
+    droptownContent.classList.toggle('droptown_menu');
+
+
+}
+function droptown_menu2(){
+    const droptownContent = document.querySelector('.inside_list2');
+
+    droptownContent.classList.toggle('droptown_menu');
+
+
+}
